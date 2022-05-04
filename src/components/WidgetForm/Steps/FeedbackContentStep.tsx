75e18bd2d28ed776a2b68e0481b63a462b19d5fd
LRamson <lucasramson@gmail.com>
2022-05-04 00:46:44 -0300

    Finalização do ScreenshotButton

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,9 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 
 import { FeedbackType, feedbackTypes } from "../";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -12,6 +14,9 @@ export function FeedbackContentStep(
     {feedbackType,
     onFeedbackRestartRequested
     }: FeedbackContentStepProps) {
+
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -40,6 +45,19 @@ export function FeedbackContentStep(
                     placeholder="Conte com detalhes o que está acontecendo..."
                 />
                 
+                <footer className="flex gap-2 mt-2">
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
+
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparente flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        Enviar feedback
+                    </button>
+                </footer>
             </form>
             </>
         )
